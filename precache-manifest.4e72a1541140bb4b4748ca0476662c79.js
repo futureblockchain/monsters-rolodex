1ab2bc65bb97f3c86b1c274704e945597f6c2578
thebenyo <b-benitez@users.noreply.github.com>
2020-05-30 19:18:30 -0500

    Updates

---
+++ precache-manifest.4e72a1541140bb4b4748ca0476662c79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1c390df805f61aa58d84c485fc6cf3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "23cdcf5ce3a34691f3c2",
+    "url": "/monsters-rolodex/static/css/main.f7840ffb.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23cdcf5ce3a34691f3c2",
+    "url": "/monsters-rolodex/static/js/main.a30ab34f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
